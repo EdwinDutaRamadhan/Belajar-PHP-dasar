4995c2d68abb187b592f52e7047b3cc7044653c6
EdwinDutaRamadhan <89692738+EdwinDutaRamadhan@users.noreply.github.com>
2022-08-31 10:49:45 +0700

    update.php

---
+++ date.php
@@ -0,0 +1,8 @@
+<?php
+    //echo date("l, d-M-y");
+    //echo time();
+    //echo date("l", time()-60*60*24*100);
+    //mktime(jam,menit,detik,bulan,tanggal,tahun)
+    //echo date("l",mktime(0,0,0,2,28,2001));
+    //echo date(strtotime("28 feb 2001"));
+?>
